03ec71be060d45cb335c77605682cd9ac2d301d8
rhayk <rhayk@DESKTOP-707USPO>
2019-10-20 14:47:57 -0300

    Fundamentos - continuação aula 32

--- Projetos_Cursos_Java_Completo/src/fundamentos/Aula_16e17_Desafio_ConversaoTemperatura.java
+++ Projetos_Cursos_Java_Completo/src/fundamentos/Aula_16e17_Desafio_ConversaoTemperatura.java
@@ -1,6 +1,6 @@
 package fundamentos;
 
-public class Aula_16_Desafio_ConversaoTemperatura {
+public class Aula_16e17_Desafio_ConversaoTemperatura {
 
 		// DESAFIO -> Como transformar temperatura Fahrenheit em Celsius?
 		// Contadores para armazenamento em ° 32 eon ° 5/9
